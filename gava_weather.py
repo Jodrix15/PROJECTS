0c7c7558e93e2ecf0a9279d51bb344abfa7a8177
Jodrix-15 <jordi.hrnsllorente@gmail.com>
2024-01-11 21:39:57 +0100

    mejoras gava_weather

--- gava_weather.py
+++ gava_weather.py
@@ -3,11 +3,19 @@ import json as js
 import sys
 from datetime import date as d
 
-def maxTemp(fecha):
+def getAPI(fecha):
+    '''obtenemos API'''
+    url = "https://archive-api.open-meteo.com/v1/archive?latitude=41.306&longitude=2.002&start_date=" + fecha + "-01&end_date=" + fecha + "-31&daily=temperature_2m_max,temperature_2m_min,rain_sum&timezone=Europe%2FBerlin"
+    return r.get(url)
+
+def getDiccionarioAPI(fecha):
+    '''esta función convierte lo que cogemos de la API a formato JSON (un diccionario)'''
+    return getAPI(fecha).json()
+
+def maxTemp(fecha, dates, tmpMaxList):
     '''Calcula la temperatura máxima del mes de la fecha que se ennvía como argumento'''
     temperaturaMaxima = 0
-    dates = data["daily"]["time"]
-    tmpMaxList = data["daily"]["temperature_2m_max"]
+
     j = 0
     for i in range(len(dates)):
         if fecha in dates[i]:
@@ -17,52 +25,54 @@ def maxTemp(fecha):
             if tmpMaxList[i] > temperaturaMaxima:
                 temperaturaMaxima = tmpMaxList[i] #obtenemos la temperaturaMaxima
 
-
     return temperaturaMaxima
 
-def minTemp(fecha):
+def minTemp(fecha, dates, tmpMinList):
     '''Calcula la temperatura mínima del mes de la fecha que se ennvía como argumento'''
     temperaturaMinima = 0
-    dates = data["daily"]["time"]
-    tmpMinList = data["daily"]["temperature_2m_min"]
+
     j = 0
     for i in range(len(dates)):
         if fecha in dates[i]:
             if j == 0:
                 temperaturaMinima = tmpMinList[i]
                 j = 1
-
             if tmpMinList[i] < temperaturaMinima:
                 temperaturaMinima = tmpMinList[i]
 
     return temperaturaMinima
 
-def totalRainyDays(fecha):
+def totalRainyDays(fecha, dates, rainList):
     '''esta funcion calcula el total de dias que ha llovido en la fecha que se envia por argumento'''
     rainyDays = 0
-    dayList = data["daily"]["time"]
-    rainList = data["daily"]["rain_sum"]
 
-    for i in range(len(dayList)):
-        if fecha in dayList[i]:
+    for i in range(len(rainList)):
+        if fecha in dates[i]:
             if rainList[i] >= 0.1:
                 rainyDays += 1
 
     return rainyDays
 
-args = sys.argv
-fecha = args[1]
+def main():
+    args = sys.argv
+    fecha = args[1]
 
-url = "https://archive-api.open-meteo.com/v1/archive?latitude=41.306&longitude=2.002&start_date="+fecha+"-01&end_date="+fecha+"-31&daily=temperature_2m_max,temperature_2m_min,rain_sum&timezone=Europe%2FBerlin"
-response = r.get(url)
-data = response.json()
+    if len(args) != 2:
+        print("No valido")
+    else:
+        data = getDiccionarioAPI(fecha)
+        dates = data["daily"]["time"]
+        tmpMaxList = data["daily"]["temperature_2m_max"]
+        tmpMinList = data["daily"]["temperature_2m_min"]
+        rainList = data["daily"]["rain_sum"]
 
-if len(args) != 2:
-    print("No valido")
-else:
-    dataTxt = js.dumps(data, indent=3)
-    print(dataTxt) #descomentar para ver lo que recogemos de la web en la terminal
-    print(f"Tmax = {maxTemp(fecha)} ºC \tTmin = {minTemp(fecha)} ºC \tDies de pluja = {totalRainyDays(fecha)}")
+        dataTxt = js.dumps(data, indent=3)
+        #print(dataTxt) #descomentar para ver lo que recogemos de la web en la terminal
+        print(f"Tmax = {maxTemp(fecha, dates, tmpMaxList)} ºC \tTmin = {minTemp(fecha, dates, tmpMinList)} ºC \tDies de pluja = {totalRainyDays(fecha, dates, rainList)}")
+
+main()
+
+
 
 
 
